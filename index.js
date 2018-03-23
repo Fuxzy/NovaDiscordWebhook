8bff374b410f29dd17ab1b140c8bff3bf00e55bc
Fuxzy <34891539+Fuxzy@users.noreply.github.com>
2018-03-23 14:57:48 -0400

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-Webhook NovaBot = new Webhook("https://discordapp.com/api/webhooks/426547895236427781/PKFGLMwl-WAQJiUdtTPVoCOsroG5DKjdJal_TnW3oM8tOEZvHvu8uzSEWf00oqAIzMfR");
-
-DiscordMessage dm = new DiscordMessage("NovaBot", "Test", "https://i.imgur.com/Y6VtrXT.png");
-NovaBot.sendMessage(dm);
