ffe60839d836ce666e8cc6c31d430e0a2e13890f
Fuxzy <34891539+Fuxzy@users.noreply.github.com>
2018-03-23 17:06:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-TemmieWebhook temmie = new TemmieWebhook("Your Webhook URL here (Get it by going to your server configurations -> Webhook");
+TemmieWebhook temmie = new TemmieWebhook("https://discordapp.com/api/webhooks/426544637327572992/UWo57d7ssh7n6l1PSixfRvMmTxme6C46DiGckIJx39Dzvq0dzj-tv20QE8LDY6JeaO-S");
 // Username, Content, Avatar URL
 DiscordMessage dm = new DiscordMessage("Temmie", "hOI, im TEMMIE!", "https://media.tenor.co/images/d205ef37ba5aad7b84fc21f6ffb36c6b/raw");
 temmie.sendMessage(dm);
